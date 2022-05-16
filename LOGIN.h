987cc77f28a1f5e289f92206b5b9381ad74b4c29
Nourhan_Mahmoud <101576070+NourhanIdc@users.noreply.github.com>
2022-05-16 03:01:19 +0200

    lib

--- LOGIN.h
+++ LOGIN.h
@@ -6,12 +6,18 @@
 #include<fstream>
 #include <regex>
 #include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <ctime>
+
 
 using namespace std;
 
 void registr();
 void login();
 void changepass();
+void forgotPassword();
 void menu();
 
 #endif //UNTITLED1_LOGIN_H
